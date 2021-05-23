39da17ab47839e00bd0fd651383da881a0e7192f
Your Name <you@example.com>
2021-05-23 16:48:42 +0000

    HH

--- a1/static/js/main.js
+++ a1/static/js/main.js
@@ -20,16 +20,18 @@ let inputText = "" ;// 入力数字
 // ______________________________
 
 function clear_click(){
+    
     inputText                   = "";
+    
     document.f1.t_input.value   = inputText ;
-    document.f1.t_1.value   = "";
-    document.f1.t_1_s.value = "";
-    document.f1.t_2.value   = "";
-    document.f1.t_2_1.value = "";
-    document.f1.t_3.value   = "";
-    document.f1.t_3_2.value = "";
-    document.f1.t_4.value   = "";
-    document.f1.t_4_3.value = "";
+    document.f1.t_1.value       = "";
+    document.f1.t_1_s.value     = "";
+    document.f1.t_2.value       = "";
+    document.f1.t_2_1.value     = "";
+    document.f1.t_3.value       = "";
+    document.f1.t_3_2.value     = "";
+    document.f1.t_4.value       = "";
+    document.f1.t_4_3.value     = "";
     
     saveTime[2] = 0; // 1
     saveTime[3] = 0; // 2
@@ -37,16 +39,16 @@ function clear_click(){
     saveTime[5] = 0; // 4
 
 }
+
 function b0_click(){
-if (inputText != "")
+if ( inputText != "" )
     {
-        if (inputText.length < 4)
+        if ( inputText.length < 4 )
         {
-            inputText = inputText + "0";
-            document.f1.t_input.value = inputText;
+             inputText = inputText + "0";
+             document.f1.t_input.value = inputText;
         }
     }   
-    
 }
 function b9_click(){
     if (inputText.length < 4)
@@ -58,43 +60,41 @@ function b9_click(){
 function b1_click(){
 if (inputText.length < 4)
 {
-    if     (inputText.length === 0 )
-    {
+    if      (inputText.length === 0 ){
         x = new Date();
         saveTime[2] = x.getTime();
         nowDate = saveTime[2];
         
-        
-        document.f1.t_1.value = saveTime[2];
+        document.f1.t_1.value   = saveTime[2];
         document.f1.t_1_s.value = saveTime[2] - saveTime[1];
- 
+        
+        
+        
+        enable_T1();
+        
+        
+        
+        
     }
-    
-    else if (inputText.length === 1 )
-    {
+    else if (inputText.length === 1 ){
         x = new Date();
         saveTime[3] = x.getTime();
         nowDate = saveTime[3];
-        
         
         document.f1.t_2.value = saveTime[3];
         document.f1.t_2_1.value = saveTime[3] - saveTime[2];
  
     }
-    
-    else if (inputText.length === 2 )
-    {
+    else if (inputText.length === 2 ){
         x = new Date();
         saveTime[4] = x.getTime();
         nowDate = saveTime[4];
         
-        
-        document.f1.t_3.value = saveTime[4];
+        document.f1.t_3.value   = saveTime[4];
         document.f1.t_3_2.value = saveTime[4] - saveTime[3];
  
     }
-    else if    (inputText.length === 3 )
-    {
+    else if (inputText.length === 3 ){
         x = new Date();
         saveTime[5] = x.getTime();
         nowDate = saveTime[5];
@@ -110,13 +110,15 @@ if (inputText.length < 4)
     }
 }
 function reset_click(){
+    document.f1.b_reset.disabled = true;
+    document.f1.t_question.value = "";
+    inputText = "";
+    nowDate = 0;
+    
 // 次やる    
     id = id + 101;
     question[0+1];
-    
-    document.f1.b_reset.disabled = true;
-    inputText = "";
-    nowDate = 0;
+//  
     
     for (let i=0; i < 9; i++){
         saveTime[i]  = 0;
@@ -146,24 +148,30 @@ function reset_click(){
     
 }
 function save_click(){
+    
     document.f1.b_save.disabled = true;
-    document.f1.t_0x.value = saveTime[0];// ID
-    document.f1.t_1x.value = saveTime[1];// start
-    document.f1.t_2x.value = saveTime[2];// 1
-    document.f1.t_3x.value = saveTime[3];// 2
-    document.f1.t_4x.value = saveTime[4];// 3
-    document.f1.t_5x.value = saveTime[5];// 4
-    document.f1.t_6x.value = saveTime[6];// enter
-//    saveTime[7]            = 1;          // true
-    document.f1.t_7x.value = saveTime[7];
-//    saveTime[8]            = inputText;  // 入力数字
-    document.f1.t_8x.value = saveTime[8];
+    document.f1.b_reset.disabled = false;
+    
+    document.f1.t_0x.value      = saveTime[0];// ID
+    document.f1.t_1x.value      = saveTime[1];// start
+    document.f1.t_2x.value      = saveTime[2];// 1
+    document.f1.t_3x.value      = saveTime[3];// 2
+    document.f1.t_4x.value      = saveTime[4];// 3
+    document.f1.t_5x.value      = saveTime[5];// 4
+    document.f1.t_6x.value      = saveTime[6];// enter
+    document.f1.t_7x.value      = saveTime[7];
+    document.f1.t_8x.value      = saveTime[8];
 }
 function enter_click(){
+    enable_F();
     document.f1.b_enter.disabled = true;
-    inputText = document.f1.t_input.value ;
-    document.f1.t_input.value = "";
+    document.f1.b_save.disabled = false;
     
+    
+    
+    inputText = document.f1.t_input.value ;
+    
+    document.f1.t_input.value = "";
     
     saveTime[0] = id;
     t = saveTime[0] - 100000;
@@ -173,23 +181,27 @@ function enter_click(){
     document.f1.t_1x.value = yy;
     saveTime[8] = inputText;  // 入力数字
 
-    
     if ( xx*yy == saveTime[8] )
     {
         saveTime[7] = 1;
     }
     
     x = new Date();
-    saveTime[6] = x.getTime();
-
+    saveTime[6]             = x.getTime();
     document.f1.t_e.value   = saveTime[6];
     document.f1.t_e_4.value = saveTime[6] - nowDate;
 }
-
 function start_click(){
-    
+    document.f1.t_input.value   = 0; //入力数字
+    enable_T();
     document.f1.b_start.disabled = true;
-    
+    document.f1.b_prep.disabled = true;
+    document.f1.clear.disabled = true;
+    document.f1.b_enter.disabled = true;
+    document.f1.b_save.disabled = true;
+    document.f1.b0.disabled = true;
+    document.f1.b_reset.disabled = true;
+
     saveTime[0] = id;
     x = new Date();
     saveTime[1] = x.getTime();
@@ -198,9 +210,53 @@ function start_click(){
     document.f1.t_question.value = question[0];
 }
 function prep_click(){
-    document.f1.b_prep.disabled = true;
-    document.f1.t_input.value = 0; //入力数字
+    enable_F();
+    document.f1.b_start.disabled = false;
 }
+function enable_T(){
+    document.f1.b0.disabled      = false;
+    document.f1.b1.disabled      = false;
+    document.f1.b2.disabled      = false;
+    document.f1.b3.disabled      = false;
+    document.f1.b4.disabled      = false;
+    document.f1.b5.disabled      = false;
+    document.f1.b6.disabled      = false;
+    document.f1.b7.disabled      = false;
+    document.f1.b8.disabled      = false;
+    document.f1.b9.disabled      = false;
+    document.f1.clear.disabled   = false;
+    document.f1.b_prep.disabled  = false;
+    document.f1.b_start.disabled = false;
+    document.f1.b_enter.disabled = false;
+    document.f1.b_save.disabled  = false;
+    document.f1.b_reset.disabled = false;
+
+}
+function enable_T1(){
+    document.f1.b0.disabled      = false;
+    document.f1.clear.disabled   = false;
+    document.f1.b_enter.disabled = false;
+    
+}
+function enable_F(){
+    document.f1.b0.disabled      = true;
+    document.f1.b1.disabled      = true;
+    document.f1.b2.disabled      = true;
+    document.f1.b3.disabled      = true;
+    document.f1.b4.disabled      = true;
+    document.f1.b5.disabled      = true;
+    document.f1.b6.disabled      = true;
+    document.f1.b7.disabled      = true;
+    document.f1.b8.disabled      = true;
+    document.f1.b9.disabled      = true;
+    document.f1.clear.disabled   = true;
+    document.f1.b_prep.disabled  = true;
+    document.f1.b_start.disabled = true;
+    document.f1.b_enter.disabled = true;
+    document.f1.b_save.disabled  = true;
+    document.f1.b_reset.disabled = true;
+}
+
 
 // _____________________________________________________________
 
