f25819ef61fcf406117dfaf8f5221c334aaed7b4
Your Name <you@example.com>
2021-05-23 13:21:33 +0000

    GG

--- a1/static/js/main.js
+++ a1/static/js/main.js
@@ -1,50 +1,45 @@
 
+// 次やる
 
-let id = 1000000;
+let id = 101101;
+// questions[];
+// 問題文の配列
+let question  = ["11x01","2x2","3x3","4x4"];
+
+
 // 二重配列　2個のカラムを持つテーブル n個
 var hairetu = new Array(2);
-for (let i = 0; i < 2 ; i++ )
-{ hairetu[i] = new Array(2).fill(0); }
-// globalと考えて良い？
-// 問題文の配列
-let question = ["100000","2x2","3x3","4x4"];
-// LAP1の保存配列 S,1,2,3,4,E
-let timeStamp = [1,2,3,4,5,6];
-// let lapStamp  = [0,0,0,0,0];
-let saveTime  = [0,0,0,0,0,0,0,0,0]; //1-9
 
+
+for (let i = 0; i < 2 ; i++ ){
+    hairetu[i] = new Array(2).fill(0); }
+
+// globalと考えて良い？
+let saveTime  = [0,0,0,0,0,0,0,0,0]; //1-9
 let nowDate   = 0;
 let inputText = "" ;// 入力数字
 
-function save_click(){
-    document.f1.t_0x.value = saveTime[0];// ID
 
-    document.f1.t_1x.value = saveTime[1];// start
-    
-    document.f1.t_2x.value = saveTime[2];// 1
-    
-    document.f1.t_3x.value = saveTime[3];// 2
-    
-    document.f1.t_4x.value = saveTime[4];// 3
-    
-    document.f1.t_5x.value = saveTime[5];// 4
-    
-    document.f1.t_6x.value = saveTime[6];// enter
-    
-    saveTime[7] = 1; // true
-    document.f1.t_7x.value = saveTime[7];
-    
-    saveTime[8] = inputText; // 入力数字
-    document.f1.t_8x.value = saveTime[8];
-    
-}
 // ______________________________
 
-function Clear_click(){
-    inputText = "";
-    document.f1.t_input.value = inputText;
+function clear_click(){
+    inputText                   = "";
+    document.f1.t_input.value   = inputText ;
+    document.f1.t_1.value   = "";
+    document.f1.t_1_s.value = "";
+    document.f1.t_2.value   = "";
+    document.f1.t_2_1.value = "";
+    document.f1.t_3.value   = "";
+    document.f1.t_3_2.value = "";
+    document.f1.t_4.value   = "";
+    document.f1.t_4_3.value = "";
+    
+    saveTime[2] = 0; // 1
+    saveTime[3] = 0; // 2
+    saveTime[4] = 0; // 3
+    saveTime[5] = 0; // 4
+
 }
-// ______________________________
 
 function b0_click(){
 if (inputText != "")
@@ -57,8 +52,6 @@ if (inputText != "")
     }   
     
 }
-// ______________________________
-
 function b9_click(){
     if (inputText.length < 4)
     {
@@ -66,7 +59,6 @@ function b9_click(){
         document.f1.t_input.value = inputText;
     }
 }
-
 function b1_click(){
 if (inputText.length < 4)
 {
@@ -121,31 +113,76 @@ if (inputText.length < 4)
     document.f1.t_input.value = inputText;
     }
 }
+function reset_click(){
+    document.f1.b_reset.disabled = true;
+    
+    inputText = "";
+    id = id + 101;
+    question[0+1];
+    nowDate = 0;
+    
+    for (let i=0; i < 9; i++){
+        saveTime[i]  = 0;
+    }
+    
+    
+    
+    
+    
+}
 
-function Enter_click(){
+function save_click(){
+    document.f1.b_save.disabled = true;
+    document.f1.t_0x.value = saveTime[0];// ID
+    document.f1.t_1x.value = saveTime[1];// start
+    document.f1.t_2x.value = saveTime[2];// 1
+    document.f1.t_3x.value = saveTime[3];// 2
+    document.f1.t_4x.value = saveTime[4];// 3
+    document.f1.t_5x.value = saveTime[5];// 4
+    document.f1.t_6x.value = saveTime[6];// enter
+//    saveTime[7]            = 1;          // true
+    document.f1.t_7x.value = saveTime[7];
+//    saveTime[8]            = inputText;  // 入力数字
+    document.f1.t_8x.value = saveTime[8];
+}
+function enter_click(){
     document.f1.b_enter.disabled = true;
     inputText = document.f1.t_input.value ;
     document.f1.t_input.value = "";
+    
+    
+    saveTime[0] = id;
+    t = saveTime[0] - 100000;
+    yy = t % 100;
+    xx = Math.floor( t*0.01 );
+    document.f1.t_0x.value = xx;
+    document.f1.t_1x.value = yy;
+    saveTime[8] = inputText;  // 入力数字
 
+    
+    if ( xx*yy == saveTime[8] )
+    {
+        saveTime[7] = 1;
+    }
+    
     x = new Date();
     saveTime[6] = x.getTime();
-//    nowDate = saveTime[6];
 
-    document.f1.t_e.value = saveTime[6];
+    document.f1.t_e.value   = saveTime[6];
     document.f1.t_e_4.value = saveTime[6] - nowDate;
 }
 
-function Start_click(){
+function start_click(){
     document.f1.b_start.disabled = true;
     saveTime[0] = id;
     x = new Date();
     saveTime[1] = x.getTime();
     document.f1.t_s.value  = saveTime[1];
     
-    document.f1.t_question.value = question[1];
+    document.f1.t_question.value = question[0];
 }
 
-function Prep_click(){
+function prep_click(){
     document.f1.b_prep.disabled = true;
     document.f1.t_input.value = 0; //入力数字
 }
