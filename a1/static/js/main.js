4f06253d8aec0cc6b4df79f43fda56232ca77e22
Your Name <you@example.com>
2021-05-24 16:28:50 +0000

    KK

--- a1/static/js/main.js
+++ a1/static/js/main.js
@@ -1,4 +1,5 @@
 
+// 整理整頓
 // 次やる
 
 let id = 101101;
@@ -6,11 +7,14 @@ let id = 101101;
 // 問題文の配列
 let question  = ["11x01","2x2","3x3","4x4"];
 
-// 二重配列　2個のカラムを持つテーブル n個
-var hairetu = new Array(2);
+// 二重配列　9個のカラムを持つテーブル 10個
 
-for (let i = 0; i < 2 ; i++ ){
-    hairetu[i] = new Array(2).fill(0); }
+let questAmount = 10;
+var hairetu = new Array(8);
+for (let i = 0; i < questAmount ; i++ ){
+    hairetu[i] = new Array(8).fill(0); }
+let ima = 0; // 今何問目？
+
 
 // globalと考えて良い？
 let saveTime  = [0,0,0,0,0,0,0,0,0]; //1-9
@@ -56,45 +60,30 @@ if (inputText.length < 4){
         x = new Date();
         saveTime[2] = x.getTime();
         nowDate = saveTime[2];
-        
         document.f1.t_1.value   = saveTime[2];
         document.f1.t_1_s.value = saveTime[2] - saveTime[1];
-        
-        
-        
         enable_T1();
-        
-        
-        
-        
     }
     else if (inputText.length === 1 ){
         x = new Date();
         saveTime[3] = x.getTime();
         nowDate = saveTime[3];
-        
         document.f1.t_2.value = saveTime[3];
         document.f1.t_2_1.value = saveTime[3] - saveTime[2];
- 
     }
     else if (inputText.length === 2 ){
         x = new Date();
         saveTime[4] = x.getTime();
         nowDate = saveTime[4];
-        
         document.f1.t_3.value   = saveTime[4];
         document.f1.t_3_2.value = saveTime[4] - saveTime[3];
- 
     }
     else if (inputText.length === 3 ){
         x = new Date();
         saveTime[5] = x.getTime();
         nowDate = saveTime[5];
-        
-        
         document.f1.t_4.value = saveTime[5];
         document.f1.t_4_3.value = saveTime[5] - saveTime[4];
- 
     }
     else{ }
     inputText = inputText + obj.value;
@@ -103,13 +92,15 @@ if (inputText.length < 4){
 }
 function reset_click(){
     document.f1.b_reset.disabled = true;
+    document.f1.b_prep.disabled = false;
+
     document.f1.t_question.value = "";
     inputText = "";
     nowDate = 0;
     
 // 次やる    
-    id = id + 101;
-    question[0+1];
+//    id = id + 101;
+//    question[0+1];
 //  
     
     for (let i=0; i < 9; i++){
@@ -138,12 +129,12 @@ function reset_click(){
     document.f1.t_7x.value  = "";
     document.f1.t_8x.value  = "";
     
+    ima += 1;
+    
 }
 function save_click(){
-    
     document.f1.b_save.disabled = true;
-    document.f1.b_reset.disabled = false;
-    
+    document.f1.b_reset.disabled= false;
     document.f1.t_0x.value      = saveTime[0];// ID
     document.f1.t_1x.value      = saveTime[1];// start
     document.f1.t_2x.value      = saveTime[2];// 1
@@ -151,18 +142,22 @@ function save_click(){
     document.f1.t_4x.value      = saveTime[4];// 3
     document.f1.t_5x.value      = saveTime[5];// 4
     document.f1.t_6x.value      = saveTime[6];// enter
-    document.f1.t_7x.value      = saveTime[7];
-    document.f1.t_8x.value      = saveTime[8];
+    document.f1.t_7x.value      = saveTime[7];// TRUE or
+    document.f1.t_8x.value      = saveTime[8];// input
+    
+
+    for (let i=0; i<9; i++){
+        hairetu[ima][i] = saveTime[i];
+    }
+    
+    
+    
 }
 function enter_click(){
     enable_F();
     document.f1.b_enter.disabled = true;
     document.f1.b_save.disabled = false;
-    
-    
-    
     inputText = document.f1.t_input.value ;
-    
     document.f1.t_input.value = "";
     
     saveTime[0] = id;
@@ -184,27 +179,28 @@ function enter_click(){
     document.f1.t_e_4.value = saveTime[6] - nowDate;
 }
 function start_click(){
-    document.f1.t_input.value   = 0; //入力数字
+    document.f1.t_input.value    = 0; //入力数字
     enable_T();
     document.f1.b_start.disabled = true;
-    document.f1.b_prep.disabled = true;
-    document.f1.clear.disabled = true;
+    document.f1.b_prep.disabled  = true;
+    document.f1.clear.disabled   = true;
     document.f1.b_enter.disabled = true;
-    document.f1.b_save.disabled = true;
-    document.f1.b0.disabled = true;
+    document.f1.b_save.disabled  = true;
+    document.f1.b0.disabled      = true;
     document.f1.b_reset.disabled = true;
 
     saveTime[0] = id;
     x = new Date();
     saveTime[1] = x.getTime();
-    document.f1.t_s.value  = saveTime[1];
+    document.f1.t_s.value        = saveTime[1];
     
-    document.f1.t_question.value = question[0];
+    document.f1.t_question.value = question[ima];
 }
 function prep_click(){
     enable_F();
     document.f1.b_start.disabled = false;
 }
+
 function enable_T(){
     document.f1.b0.disabled      = false;
     document.f1.b1.disabled      = false;
