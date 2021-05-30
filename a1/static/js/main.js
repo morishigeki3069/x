eec5efbfd907495612988da26233a9f11e6acc66
Your Name <you@example.com>
2021-05-30 08:02:54 +0000

    GGG

--- a1/static/js/main.js
+++ a1/static/js/main.js
@@ -1,18 +1,11 @@
-
 // 整理整頓
-const qsLength  = 4;      // 問題数
-
-//var saveTime  = [0,0,0,0,0,0,0,0,0]; //1-9
-
+const qsLength  = 4;// 問題数
 let inputText = "" ;// 入力数字
 let ima       = 0;
-let nowDate   = 0;
 let nowTime   = 0;
 let questions = [];
 
-
 // ２次元配列
-
 var saveData = new Array(1);        // 必須１行目　この方法しかない？？
 for (let i=0; i< qsLength; i++)
 { saveData[i] = new Array(9).fill(0); } // カラムの数
@@ -39,7 +32,6 @@ function qu_generator(idNum){
     dw = String(yy);
     
     return up + "x" + dw;}
-    
 function correct(){
     
     id = questions[ima];
@@ -48,12 +40,11 @@ function correct(){
     xx = Math.floor(x * 0.01);
     if ( inputText == String(xx * yy))
     {
-        return 88888888888 ;    
+        return 99999 ;    
     }
     else
     {
         return 0 ;
-        
     }
 }
 // ______________________________
@@ -65,18 +56,12 @@ function prep_click(){
 function start_click(){
     start_enable();
     document.f1.t_input.value    = 0; //入力数字
-    
-//    saveTime[0] = questions[ima];// 後で消す
     saveData[ima][0] = questions[ima];
     
     x = new Date();
-//    saveTime[1]      = x.getTime();// 後で消す
     saveData[ima][1] = x.getTime();
-    
-//    document.f1.t_s.value        = saveTime[1];// 後で消す
     document.f1.t_s.value        = saveData[ima][1];
 
-    document.f1.t_test.value     = questions[ima];
     document.f1.t_question.value = qu_generator(questions[ima]);
     document.f1.t_remain.value   = qsLength - ima; // 残数に利用
 }
@@ -84,19 +69,11 @@ function b1_click(obj){
     if (inputText.length < 4){
         if      (inputText.length === 0 ){
             x = new Date();
-//            saveTime[2]      = x.getTime();// 後で消す
             saveData[ima][2] = x.getTime();
-            
-            
-//            nowDate = saveTime[2];// 後で消す
             nowTime = saveData[ima][2];
 
-            
-            
-//            document.f1.t_1.value   = saveTime[2];// 後で消す
             document.f1.t_1.value   = saveData[ima][2];
             
-//            document.f1.t_1_s.value = saveTime[2] - saveTime[1];// 後で消す
             document.f1.t_1_s.value = saveData[ima][2] - saveData[ima][1];
             
             
@@ -105,62 +82,29 @@ function b1_click(obj){
         }
         else if (inputText.length === 1 ){
             x = new Date();
- //           saveTime[3]      = x.getTime();// 後で消す
             saveData[ima][3] = x.getTime();
             
-            
- //           nowDate = saveTime[3];
             nowTime = saveData[ima][3];
             
-            
-            
-            
- //           document.f1.t_2.value = saveTime[3];// 後で消す
             document.f1.t_2.value = saveData[ima][3];
             
-            
- //           document.f1.t_2_1.value = saveTime[3] - saveTime[2];// 後で消す
             document.f1.t_2_1.value = saveData[ima][3] - saveData[ima][2];
             
         }
         else if (inputText.length === 2 ){
             x = new Date();
-  //          saveTime[4] = x.getTime();// 後で消す
             saveData[ima][4] = x.getTime();
-            
- //           nowDate = saveTime[4];// 後で消す
             nowTime = saveData[ima][4];
-            
-            
-            
- //           document.f1.t_3.value   = saveTime[4];// 後で消す
             document.f1.t_3.value   = saveData[ima][4];
-            
-            
-            
- //           document.f1.t_3_2.value = saveTime[4] - saveTime[3];// 後で消す
             document.f1.t_3_2.value = saveData[ima][4] - saveData[ima][3];
             
         }
         else if (inputText.length === 3 ){
             x = new Date();
- //           saveTime[5] = x.getTime();// 後で
             saveData[ima][5] = x.getTime();
-
- //           nowDate = saveTime[5];// 後で
             nowTime = saveData[ima][5];
-            
-            
-            
-            
- //           document.f1.t_4.value = saveTime[5];// 後で
             document.f1.t_4.value = saveData[ima][5];
-            
-            
-            
- //           document.f1.t_4_3.value = saveTime[5] - saveTime[4];// 後で
             document.f1.t_4_3.value = saveData[ima][5] - saveData[ima][4];
-            
             
         }
         else{ }
@@ -182,12 +126,6 @@ function clear_click(){
     document.f1.t_3_2.value     = "";
     document.f1.t_4.value       = "";
     document.f1.t_4_3.value     = "";
- /*   
-    saveTime[2] = 0; // 1
-    saveTime[3] = 0; // 2
-    saveTime[4] = 0; // 3
-    saveTime[5] = 0; // 4
-*/
     saveData[ima][2] = 0; // 1
     saveData[ima][3] = 0; // 2
     saveData[ima][4] = 0; // 3
@@ -197,38 +135,17 @@ function clear_click(){
 
 }
 function enter_click(){
-         enter_enable();
-//    saveTime[7] = correct();            // 後で消す
-    saveData[ima][7] = correct();
- //   saveTime[8] = inputText;            // 入力数字
-    saveData[ima][8] = inputText;        
+    enter_enable();
     
     x = new Date();
-//    saveTime[6]      = x.getTime();// 後で消す
     saveData[ima][6] = x.getTime();        
+    saveData[ima][7] = correct();
+    saveData[ima][8] = inputText;        
     
-//    document.f1.t_e.value   = saveTime[6];
-    document.f1.t_e.value   = saveData[ima][6];
-
-
-
-//    document.f1.t_e_4.value = saveTime[6] - nowDate;
-    document.f1.t_e_4.value = saveData[ima][6] - nowTime;
-    
-
     enter_document();    
-
 }
 function save_click(){
     save_enable();
-
-// いらない？   
-//    for (let i=0; i<9; i++)
-//    {    hairetu[ima][i] = saveTime[i];}
-//
-    
-    
-    
 }
 function reset_click(){
     reset_enable();
@@ -246,17 +163,12 @@ function reset_click(){
     
 // 変数クリア
     inputText = "";
-    nowDate = 0;
-    
+    nowTime = 0;
     
     
     clear_document();
 }
 function next_click(){
-
-//いらない？？  saveTime のリセット 
- //   for (let i=0; i < 9; i++){ saveTime[i]  = 0; }
-//
 
 }
 function test_click(){
@@ -391,7 +303,6 @@ function clear_document(){
     
     document.f1.t_input.value   = "";
     document.f1.t_question.value= "";
-    document.f1.t_test.value    = "";
     document.f1.t_remain.value  = "";
 
     document.f1.t_s.value   = "";
@@ -419,27 +330,7 @@ function clear_document(){
     
 }
 function enter_document(){
-    /*
-    document.f1.t_0x.value = saveTime[0];// ID
-    document.f1.t_1x.value = saveTime[1];// start
-    document.f1.t_2x.value = saveTime[2];// 1
-    document.f1.t_3x.value = saveTime[3];// 2
-    document.f1.t_4x.value = saveTime[4];// 3
-    document.f1.t_5x.value = saveTime[5];// 4
-    document.f1.t_6x.value = saveTime[6];// enter
-    document.f1.t_7x.value = saveTime[7];// TRUE or
-    document.f1.t_8x.value = saveTime[8];// input
-*/
-    document.f1.t_0x.value = saveData[ima][0];// ID
-    document.f1.t_1x.value = saveData[ima][1];// start
-    document.f1.t_2x.value = saveData[ima][2];// 1
-    document.f1.t_3x.value = saveData[ima][3];// 2
-    document.f1.t_4x.value = saveData[ima][4];// 3
-    document.f1.t_5x.value = saveData[ima][5];// 4
-    document.f1.t_6x.value = saveData[ima][6];// enter
-    document.f1.t_7x.value = saveData[ima][7];// TRUE or
-    document.f1.t_8x.value = saveData[ima][8];// input
-    
+    document.f1.t_e.value   = saveData[ima][6];
+    document.f1.t_e_4.value = saveData[ima][6] - nowTime;
 }
 // _____________________________________________________________
-
